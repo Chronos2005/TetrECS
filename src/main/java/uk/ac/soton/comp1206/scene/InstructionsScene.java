1eff68107ca0719c99cd7bb7c4df672f8bde8b04
Ross <rossmelo2005@gmail.com>
2024-04-11 16:00:40 +0100

    Made improvements to the instructions scene

--- src/main/java/uk/ac/soton/comp1206/scene/InstructionsScene.java
+++ src/main/java/uk/ac/soton/comp1206/scene/InstructionsScene.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -35,38 +36,55 @@ public class InstructionsScene extends BaseScene {
 
     public void build() {
         logger.info("Building " + this.getClass().getName());
-        var instructionsPane = new StackPane();
+    var instructionsPane = new BorderPane();
         root = new GamePane(gameWindow.getWidth(),gameWindow.getHeight());
 
         instructionsPane.setMaxWidth(gameWindow.getWidth());
         instructionsPane.setMaxHeight(gameWindow.getHeight());
         instructionsPane.getStyleClass().add("menu-background");
-        Grid grid = new Grid(3,3);
-        //creating the 15 pieces and
-        //Pieceboard 1
-        PieceBoard pieceBoard1= new PieceBoard(grid,gameWindow.getWidth()/12,gameWindow.getHeight()/12);
-        pieceBoard1.settingPieceToDisplay(GamePiece.createPiece(0));
-        //PieceBoard2
-        PieceBoard pieceBoard2= new PieceBoard(grid,gameWindow.getWidth()/12,gameWindow.getHeight()/12);
-        pieceBoard2.settingPieceToDisplay(GamePiece.createPiece(1));
-        PieceBoard pieceBoard3= new PieceBoard(grid,gameWindow.getWidth()/12,gameWindow.getHeight()/12);
-        PieceBoard pieceBoard4= new PieceBoard(grid,gameWindow.getWidth()/12,gameWindow.getHeight()/12);
 
         //Adding Background image
         Image image = new Image(Objects.requireNonNull(InstructionsScene.class.getResource("/images/Instructions.png")).toExternalForm());
         ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(gameWindow.getHeight());
-        imageView.setFitWidth(gameWindow.getWidth());
-        instructionsPane.getChildren().addAll(imageView,pieceBoard1,pieceBoard2);
+        imageView.setFitHeight(gameWindow.getHeight()/2);
+        imageView.setPreserveRatio(true); // maintain aspect ratio
+        // Create a StackPane to center the ImageView
+        StackPane imagePane = new StackPane(imageView);
+        BorderPane.setAlignment(imagePane, Pos.TOP_CENTER); // Align the StackPane to the top center
+
+        instructionsPane.setTop(imagePane);
+        createBlocks().setAlignment(Pos.CENTER);
+        instructionsPane.setBottom(createBlocks());
         root.getChildren().add(instructionsPane);
 
+    }
 
+    public VBox createBlocks(){
+        HBox hBox1 = new HBox();
+        HBox hBox2 = new HBox();
+        HBox hBox3 = new HBox();
+        VBox display = new VBox();
+        for (var x=0;x<5;x++){
+            Grid grid = new Grid(3,3);
+            PieceBoard pieceBoard = new PieceBoard(grid,gameWindow.getWidth()/12,gameWindow.getHeight()/12);
+            pieceBoard.settingPieceToDisplay(GamePiece.createPiece(x));
+            hBox1.getChildren().add(pieceBoard);
 
-
-
-
-
-
+        }
+        for (var x=5; x<10;x++){
+            Grid grid = new Grid(3,3);
+            PieceBoard pieceBoard = new PieceBoard(grid,gameWindow.getWidth()/12,gameWindow.getHeight()/12);
+            pieceBoard.settingPieceToDisplay(GamePiece.createPiece(x));
+            hBox2.getChildren().add(pieceBoard);
+        }
+        for (var x=10; x<15;x++){
+            Grid grid = new Grid(3,3);
+            PieceBoard pieceBoard = new PieceBoard(grid,gameWindow.getWidth()/12,gameWindow.getHeight()/12);
+            pieceBoard.settingPieceToDisplay(GamePiece.createPiece(x));
+            hBox3.getChildren().add(pieceBoard);
+        }
+        display.getChildren().addAll(hBox1,hBox2,hBox3);
+        return display;
     }
 
 }
