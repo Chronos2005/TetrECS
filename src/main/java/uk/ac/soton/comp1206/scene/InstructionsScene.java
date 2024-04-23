bf21c33d2853d960c53568b7f004bd6aa4460b68
Ross <rossmelo2005@gmail.com>
2024-04-23 16:08:52 +0100

    Finished the instructions page

--- src/main/java/uk/ac/soton/comp1206/scene/InstructionsScene.java
+++ src/main/java/uk/ac/soton/comp1206/scene/InstructionsScene.java
@@ -5,10 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -50,43 +47,60 @@ public class InstructionsScene extends BaseScene {
     ImageView imageView = new ImageView(image);
     imageView.setFitHeight(gameWindow.getHeight() / 2);
     imageView.setPreserveRatio(true); // maintain aspect ratio
-    // Create a StackPane to center the ImageView
+    // Create a StackPane to center the ImageView'
+    Text Title = new Text("Instructions");
+    Title.getStyleClass().add("title");
+    Text instructions = new Text("TertECS is a fast-paced gravity- free block placement game, where you must survive by clearing rows through careful placement of the \nupcoming  blocks before the time runs out. Loose all three lives and you're destroyes!");
+    instructions.setWrappingWidth(gameWindow.getWidth()/2);
+    instructions.getStyleClass().add("instructions");
     StackPane imagePane = new StackPane(imageView);
-    BorderPane.setAlignment(imagePane, Pos.TOP_CENTER); // Align the StackPane to the top center
+    VBox display = new VBox(Title,instructions,imagePane);
+    display.setAlignment(Pos.CENTER);
+    BorderPane.setAlignment(display, Pos.TOP_CENTER); // Align the StackPane to the top center
+    var gamePieces = createBlocks();
+    instructionsPane.setTop(display);
+    instructionsPane.setBottom(gamePieces);
+    BorderPane.setAlignment(gamePieces, Pos.CENTER);
 
-    instructionsPane.setTop(imagePane);
-    createBlocks().setAlignment(Pos.CENTER);
-    instructionsPane.setBottom(createBlocks());
     root.getChildren().add(instructionsPane);
   }
 
   public VBox createBlocks() {
-    HBox hBox1 = new HBox();
-    HBox hBox2 = new HBox();
-    HBox hBox3 = new HBox();
+    Text text = new Text("Game Pieces");
+    text.getStyleClass().add("title");
+    GridPane gridPane = new GridPane();
     VBox display = new VBox();
+    display.setAlignment(Pos.CENTER);
+
+
     for (var x = 0; x < 5; x++) {
       Grid grid = new Grid(3, 3);
       PieceBoard pieceBoard =
           new PieceBoard(grid, gameWindow.getWidth() / 12, gameWindow.getHeight() / 12);
       pieceBoard.settingPieceToDisplay(GamePiece.createPiece(x));
-      hBox1.getChildren().add(pieceBoard);
+
+      gridPane.add(pieceBoard, x, 0);
     }
     for (var x = 5; x < 10; x++) {
       Grid grid = new Grid(3, 3);
       PieceBoard pieceBoard =
           new PieceBoard(grid, gameWindow.getWidth() / 12, gameWindow.getHeight() / 12);
       pieceBoard.settingPieceToDisplay(GamePiece.createPiece(x));
-      hBox2.getChildren().add(pieceBoard);
+      gridPane.add(pieceBoard, x - 5, 1);
     }
     for (var x = 10; x < 15; x++) {
       Grid grid = new Grid(3, 3);
       PieceBoard pieceBoard =
           new PieceBoard(grid, gameWindow.getWidth() / 12, gameWindow.getHeight() / 12);
       pieceBoard.settingPieceToDisplay(GamePiece.createPiece(x));
-      hBox3.getChildren().add(pieceBoard);
+        gridPane.add(pieceBoard, x - 10, 2);
     }
-    display.getChildren().addAll(hBox1, hBox2, hBox3);
+
+    // Set horizontal and vertical gap
+    gridPane.setHgap(10); // horizontal gap in pixels
+    gridPane.setVgap(10); // vertical gap in pixels
+    gridPane.setAlignment(Pos.CENTER);
+    display.getChildren().addAll(text,gridPane);
     return display;
   }
 }
